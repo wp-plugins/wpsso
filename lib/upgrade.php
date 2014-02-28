7b6fd23b97c0a1d58728391660e5d61dc1365ba5
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-28 19:28:08 +0000

    v2.2rc1
    git-svn-id: https://plugins.svn.wordpress.org/wpsso/tags/2.2rc1@867153 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/upgrade.php
@@ -0,0 +1,37 @@
+<?php
+/*
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.txt
+Copyright 2012-2014 - Jean-Sebastien Morisset - http://surniaulula.com/
+*/
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'These aren\'t the droids you\'re looking for...' );
+
+if ( ! class_exists( 'WpssoOptionsUpgrade' ) && class_exists( 'WpssoOptions' ) ) {
+
+	class WpssoOptionsUpgrade extends WpssoOptions {
+
+		private $renamed_site_keys = array();
+
+		private $renamed_keys = array(
+			'og_img_resize' => 'plugin_auto_img_resize',
+		);
+
+		protected $p;
+
+		public function __construct( &$plugin ) {
+			$this->p =& $plugin;
+			$this->p->debug->mark();
+		}
+
+		// def_opts accepts output from functions, so don't force reference
+		public function options( $options_name, &$opts = array(), $def_opts = array() ) {
+			$opts = $this->p->util->rename_keys( $opts, $this->renamed_keys );
+			$opts = $this->sanitize( $opts, $def_opts );	// cleanup excess options and sanitize
+			return $opts;
+		}
+	}
+}
+
+?>
