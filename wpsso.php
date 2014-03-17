e8c71c929c3d3b781cb9ca9698d1ed409874ab59
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-17 13:20:10 +0000

    v2.3.2
    git-svn-id: https://plugins.svn.wordpress.org/wpsso/trunk@876576 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpsso.php
+++ wpsso.php
@@ -6,7 +6,7 @@ Author: Jean-Sebastien Morisset
 Author URI: http://surniaulula.com/
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl.txt
-Description: Improves Ranking and Click-Through-Rate (CTR) on Social Websites and Google Search — A Fast and Full Featured Plugin! 
+Description: Improves Ranking and Click-Through-Rate (CTR) on Social Websites and Google Search - A Fast and Full Featured Plugin! 
 Version: 2.3.2
 
 Copyright 2012-2014 - Jean-Sebastien Morisset - http://surniaulula.com/
