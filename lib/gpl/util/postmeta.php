c13338f78118d540c9d9a9522fdede4082647d36
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-11 20:36:01 +0000

    v2.5.0dev2
    git-svn-id: https://plugins.svn.wordpress.org/wpsso/trunk@930614 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/gpl/util/postmeta.php
+++ lib/gpl/util/postmeta.php
@@ -11,19 +11,6 @@ if ( ! defined( 'ABSPATH' ) )
 if ( ! class_exists( 'WpssoUtilPostmeta' ) && class_exists( 'WpssoPostmeta' ) ) {
 
 	class WpssoUtilPostmeta extends WpssoPostmeta {
-
-		// parent __construct calls add_actions()
-		protected function add_actions() {
-			if ( ! is_admin() )
-				return;
-
-			if ( $this->p->is_avail['opengraph'] )
-				add_action( 'admin_head', array( &$this, 'set_header_tags' ) );
-
-			add_action( 'add_meta_boxes', array( &$this, 'add_metaboxes' ) );
-			add_action( 'save_post', array( &$this, 'flush_cache' ), 20 );
-			add_action( 'edit_attachment', array( &$this, 'flush_cache' ), 20 );
-		}
 	}
 }
 
--- lib/gpl/util/user.php
+++ lib/gpl/util/user.php
@@ -11,23 +11,6 @@ if ( ! defined( 'ABSPATH' ) )
 if ( ! class_exists( 'WpssoUtilUser' ) && class_exists( 'WpssoUser' ) ) {
 
 	class WpssoUtilUser extends WpssoUser {
-
-		// parent __construct calls add_actions()
-		protected function add_actions() {
-			add_filter( 'user_contactmethods', array( &$this, 'add_contact_methods' ), 20, 1 );
-
-			if ( ! is_admin() )
-				return;
-
-			if ( $this->p->is_avail['opengraph'] )
-				add_action( 'admin_head', array( &$this, 'set_header_tags' ) );
-
-			add_action( 'admin_init', array( &$this, 'add_metaboxes' ) );
-			add_action( 'show_user_profile', array( $this, 'show_metabox' ) );
-			add_action( 'edit_user_profile', array( $this, 'show_metabox' ) );
-			add_action( 'edit_user_profile_update', array( &$this, 'sanitize_contact_methods' ) );
-			add_action( 'personal_options_update', array( &$this, 'sanitize_contact_methods' ) ); 
-		}
 	}
 }
 
