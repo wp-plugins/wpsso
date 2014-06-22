88c0939eb7dbb78446838f4affa505f273ec70f5
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-22 19:01:12 +0000

    v2.5.2dev2
    git-svn-id: https://plugins.svn.wordpress.org/wpsso/trunk@936592 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/config.php
+++ lib/config.php
@@ -327,7 +327,6 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 				'review' => 'http://wordpress.org/support/view/plugin-reviews/wpsso#postform',
 				'readme' => 'http://plugins.svn.wordpress.org/wpsso/trunk/readme.txt',
 				'setup' => 'http://plugins.svn.wordpress.org/wpsso/trunk/setup.html',
-				'whatsnew' => 'http://plugins.svn.wordpress.org/wpsso/trunk/whatsnew.html',
 				'changelog' => 'http://surniaulula.com/extend/plugins/wpsso/changelog/',
 				'purchase' => 'http://surniaulula.com/extend/plugins/wpsso/',
 				'codex' => 'http://surniaulula.com/codex/plugins/wpsso/',
