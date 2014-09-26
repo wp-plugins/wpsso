c08f4d82ad4b2c922a7ed48f3d3c0c21a9ed0c6f
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-26 16:35:36 +0000

    v2.6.6
    git-svn-id: https://plugins.svn.wordpress.org/wpsso/trunk@997275 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/config.php
+++ lib/config.php
@@ -135,6 +135,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 						'download' => 'https://wordpress.org/plugins/wpsso-ssb/',
 					),
 				),
+				/*
 				'wpssoam' => array(
 					'short' => 'WPSSO AM',		// short plugin name
 					'name' => 'WPSSO App Meta (WPSSO AM)',
@@ -147,6 +148,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 						'download' => 'https://wordpress.org/plugins/wpsso-am/',
 					),
 				),
+				*/
 			),
 			'opt' => array(				// options
 				'version' => 301,		// increment when changing default options
