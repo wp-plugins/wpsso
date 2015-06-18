a1ae0b4139fdb5f54600193d7c274ed67851b5fa
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-18 02:07:58 +0000

    v3.4 fine-tune false positive detection
    git-svn-id: https://plugins.svn.wordpress.org/wpsso/tags/3.4@1183051 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/check.php
+++ lib/check.php
@@ -294,7 +294,7 @@ if ( ! class_exists( 'WpssoCheck' ) ) {
 					$wpseo_notif = json_decode( $wpseo_notif );
 					if ( ! empty( $wpseo_notif ) ) {
 						foreach ( $wpseo_notif as $num => $msgs ) {
-							if ( $msgs->type == 'error' && strpos( $msgs->message, $plugin_name ) !== false ) {
+							if ( $msgs->options->type == 'error' && strpos( $msgs->message, $plugin_name ) !== false ) {
 								unset( $wpseo_notif[$num] );
 								set_transient( Yoast_Notification_Center::TRANSIENT_KEY,
 									json_encode( $wpseo_notif ) );
